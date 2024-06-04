a0eac62cebb6320ff42e4459afd2bc9095f7755f
David Rada <davidrada@davids-mbp.lan>
2024-06-04 14:45:23 -0400

    Added router and home page

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+            }}
+        >
+            <h1>
+                Chess API
+            </h1>
+        </div>
+    );
+};
+
+export default Home;
